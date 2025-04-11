f5964f359ebdc7aab2a66c10d8a6e3a62038ca39
alardos <szilard.pal2014@gmail.com>
2025-04-11 19:42:06 +0200

    feat: minimal text editing functionality

--- src/app/component/text-card/text-card.component.ts
+++ src/app/component/text-card/text-card.component.ts
@@ -34,7 +34,6 @@ export class TextCardComponent implements OnInit {
     }
 
     thisCardColor(): string {
-        console.log(this.members,this.data);
         return this.members.find(m => m.id === this.data.owner)?.color ?? "#FFFFFF"
     }
     textareaKeydown() {
@@ -55,5 +54,63 @@ export class TextCardComponent implements OnInit {
     }
 
     edit() { this.mode = 'edit'; }
+
+    bold() {
+        this.replaceSelection(selected => {
+            if (selected.startsWith("**") && selected.endsWith("**"))
+                return selected.substring(2).substring(0,selected.length-4)
+            else
+                return `**${selected}**`
+        })
+    }
+
+    italic() {
+        this.replaceSelection(selected => {
+            if (selected.startsWith("*") && selected.endsWith("*"))
+                return selected.substring(1).substring(0,selected.length-2)
+            else
+                return `*${selected}*`
+        })
+    }
+    header() {
+        this.togglePrefix("# ")
+    }
+    bullet() {
+        this.togglePrefix("- ")
+    }
+    numbered() {
+        this.togglePrefix("1. ")
+    }
+
+    togglePrefix(prefix: string) {
+        const textarea: any = this.textareaRef.nativeElement;
+        let start = textarea.selectionStart;
+        let value = textarea.value as string;
+        const beforeCursor = value.substring(0, start);
+        const lineStart = beforeCursor.lastIndexOf('\n') + 1;
+
+        if (value.substring(lineStart).startsWith(prefix)) {
+            value = value.substring(0, lineStart) + value.substring(lineStart+prefix.length)
+            start = start-prefix.length;
+        } else {
+            value = value.substring(0, lineStart) + prefix + value.substring(lineStart)
+            start = start+prefix.length;
+        }
+
+        textarea.value = value;
+        textarea.selectionStart = start;
+    }
+
+    replaceSelection(change: (og: string) => string) {
+        const textarea: any = this.textareaRef.nativeElement;
+        const start = textarea.selectionStart;
+        const end = textarea.selectionEnd;
+        const selectedText = textarea.value.substring(start, end);
+
+        if (selectedText) {
+            const newText = textarea.value.substring(0, start) + change(selectedText) + textarea.value.substring(end);
+            textarea.value = newText;
+        }
+    }
 
 }
